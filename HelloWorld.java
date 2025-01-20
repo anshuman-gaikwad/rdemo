cfc4c467bdfa747c46148ba8f979a222dfdc9c1c
anshuman-gaikwad <anshuman.gaikwad_24uds@sanjivani.edu.in>
2025-01-20 14:04:34 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,3 +4,10 @@ public class HelloWorld{
     System.out.println("Anshu RHony");
   }
 }
+Programming: Python, R, Java, C++, SQL
+Machine Learning and AI Frameworks: TensorFlow, PyTorch, Scikit-learn, Keras
+Big Data Tools: Hadoop, Spark, Kafka, NoSQL
+Data Visualization: Tableau, Power BI, Matplotlib, Seaborn
+Cloud Platforms: AWS, Google Cloud, Azure
+Mathematics and Statistics: Probability, linear algebra, optimization techniques
+Soft Skills: Problem-solving, communication, business acumen, teamwork
